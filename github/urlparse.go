9d15c728e45eb2f446580da0956e874958bff315
kvnxiao <xlzqwerty1@gmail.com>
2019-02-24 20:24:22 -0800

    add parsing util for converting github.io to github repo name

---
+++ github/urlparse.go
@@ -0,0 +1,26 @@
+package github
+
+import (
+	"errors"
+	"strings"
+)
+
+func GetApiEndpoint(hostname string, path string) string {
+	if hostname == HostName {
+		return GetReposEndpoint(strings.TrimRight(path, "/"))
+	} else if strings.HasSuffix(hostname, ".github.io") {
+		repoPath, err := convertGitHubIOToGitHubRepo(hostname, path)
+		if err == nil {
+			return GetReposEndpoint(repoPath)
+		}
+	}
+	return ""
+}
+
+func convertGitHubIOToGitHubRepo(hostname string, path string) (string, error) {
+	if path == "" || path == "/" {
+		return "", errors.New("cannot parse a root github.io link without additional path")
+	}
+	user := hostname[:strings.Index(hostname, ".")]
+	return user + strings.TrimRight(path, "/"), nil
+}
