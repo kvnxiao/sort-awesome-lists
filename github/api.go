98f35cb6ebfba3c6a5e4fb7a4b1eda1d7318fc58
kvnxiao <xlzqwerty1@gmail.com>
2019-02-24 20:52:16 -0800

    add retry

--- github/api.go
+++ github/api.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/kvnxiao/sort-awesome-lists/logging"
@@ -42,10 +43,20 @@ func fetchRepoJson(repoURL string, token string) Repository {
 }
 
 func GetRepoStars(repoURL string, token string) int {
+	return getRepoStars(repoURL, token, 5)
+}
+
+func getRepoStars(repoURL string, token string, retries int) int {
 	repo := fetchRepoJson(repoURL, token)
-	if repo.Message != "" {
-		time.Sleep(1000 * time.Millisecond)
-		return GetRepoStars(repoURL, token)
+	if repo.Message != "" && repo.Message != "Not Found" {
+		if retries > 0 {
+			logging.Printlnf("temporary error message for repo %s: %s. Retrying...", repoURL, repo.Message)
+			time.Sleep(500 * time.Millisecond)
+			return getRepoStars(repoURL, token, retries-1)
+		} else {
+			fmt.Printf("failed to retrieve stats for %s after 5 retries", repoURL)
+			return 0
+		}
 	}
 	return repo.StargazersCount
 }
